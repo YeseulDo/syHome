2119426ba019f305e82cbb4894486fc0fb301d1a
YeseulDo <rbshine12@gmail.com>
2022-02-26 19:51:17 +0900

    init

---
+++ src/main/java/com/yeseul/syHome/SyHomeApplication.java
@@ -0,0 +1,13 @@
+package com.yeseul.syHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeseul/syHome/SyHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeseul.syHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
